e08af2a93b884afb5ae76b21e6ef4c3273ef9330
Maartje Eyskens <meyskens@me.com>
2017-11-23 10:44:09 +0100

    use credentials

--- data/mongodb.js
+++ data/mongodb.js
@@ -1,3 +1,3 @@
 const mongoose = require("mongoose")
 mongoose.set("debug", true) // Hey this is not prodution!
-module.exports = mongoose.createConnection(process.env.MONGODB) // Pass connection onwards
+module.exports = mongoose.createConnection(`mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASS}@${process.env.MONGOHOST}:${process.env.MONGOPORT}/${process.env.MONGODB}`) // Pass connection onwards
