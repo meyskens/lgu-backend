970032306c0a32683569a63d3cec048261cbc956
Maartje Eyskens <meyskens@me.com>
2017-11-23 14:20:58 +0100

    use nodejs promises

--- data/mongodb.js
+++ data/mongodb.js
@@ -1,3 +1,4 @@
 const mongoose = require("mongoose")
 mongoose.set("debug", true) // Hey this is not prodution!
+mongoose.Promise = global.Promise;
 module.exports = mongoose.createConnection(`mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASS}@${process.env.MONGOHOST}:${process.env.MONGOPORT}/${process.env.MONGODB}`) // Pass connection onwards
