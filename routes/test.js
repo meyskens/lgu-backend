d24df7473b2bc91255b1cb68f9fbf050d29a65cf
Maartje Eyskens <meyskens@me.com>
2017-11-24 10:13:39 +0100

    delete test

--- routes/test.js
+++
@@ -1,8 +0,0 @@
-const sms = require("../services/sms")
-
-module.exports = ({ app, wrap }) => {
-    app.get("/v1/test/sms", wrap(async(req, res) => {
-        await sms.send("+32498645061", "Hello form LGU. Your reservation of BEMT Japan is declined")
-        return res.send("ok")
-    }))
-}
