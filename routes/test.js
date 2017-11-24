be08162c6506f3e54e75d50338aa6c0a36d9ac34
Maartje Eyskens <meyskens@me.com>
2017-11-24 14:34:46 +0100

    no test

--- routes/test.js
+++
@@ -1,7 +0,0 @@
-const email = require("../services/email")
-
-module.exports = ({ app, wrap }) => {
-    app.get("/v1/test", async (req, res) => {
-        await email.send("maartje@eyskens.me", "hello", "Java sucks!")
-    })
-}
